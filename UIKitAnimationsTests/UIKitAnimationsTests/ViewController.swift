4e55a9ce1739ba9e30dddea26d7d62e1d89b8f43
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-04-23 18:12:01 +0100

    uikit animations first commit

---
+++ UIKitAnimationsTests/UIKitAnimationsTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKitAnimationsTests
+//
+//  Created by GEORGE QUENTIN on 23/04/2016.
+//  Copyright © 2016 GEORGE QUENTIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
